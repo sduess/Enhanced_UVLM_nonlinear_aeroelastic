e2295c5c9216478dcd351f3ac776fb3c926014ce
Stefanie Düssler <s.dussler20@imperial.ac.uk>
2023-07-25 10:07:28 +0800

    fix [gust response] color for plots
    - same gust lengths have same color

--- 04_Gust_Response/plot_gust_response.py
+++ 04_Gust_Response/plot_gust_response.py
@@ -6,16 +6,18 @@ route_dir = os.path.abspath(os.path.dirname(os.path.realpath(__file__)))
 
 def plot_data(list_gust_lengths, list_gust_intensity, list_cfl1, list_polars, case_string_format,results_folder):
     fig, axs = plt.subplots(4)
-    for gust_length in list_gust_lengths:
+    for i_gust_length, gust_length in enumerate(list_gust_lengths):
         for gust_intensity in list_gust_intensity:
             for cfl1 in list_cfl1:
                 for polars in list_polars:
                     file = results_folder +case_string_format.format(gust_length, gust_intensity, polars, cfl1) + '.txt'
                     data = np.loadtxt(file,
                                       delimiter=",")
+                    data[1:,1] /= 2
                     for iparameter in range(np.shape(data)[1] - 1):
                         axs[iparameter].plot(data[1:,0], 
                                              data[1:,iparameter + 1], 
+                                             color = list_colors[i_gust_length],
                                              linestyle = get_linestyle(cfl1, polars), 
                                              label= get_label(cfl1, polars, iparameter, gust_length))
     for iax in range(len(axs)):
@@ -48,8 +50,8 @@ def main():
 
     list_gust_lengths = [5, 10, 20, 40, 80, 100]
     list_gust_intensity = [10]
-    list_cfl1 = [1, 0] #[0, 1]
-    list_polars = [0] #[0, 1]
+    list_cfl1 = [0, 1] 
+    list_polars = [0] 
     plot_data(list_gust_lengths, list_gust_intensity, list_cfl1, list_polars, case_string_format, results_folder)
 
 
@@ -57,5 +59,6 @@ def main():
 
 if __name__ == '__main__':
     list_ylabels = ['$z_{tip}/s$, %', '$M_{OOP}$, Nm$^2$', '$M_{T}$, Nm$^2$', '$\Theta$, deg']
+    list_colors = ['tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple', 'tab:brown']
     list_linestyles = ['-', '--', ':']
     main()
