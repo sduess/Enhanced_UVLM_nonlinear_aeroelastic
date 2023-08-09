2a7e8683c15c406fcfa37c2e3361f447e5ec1ae6
Stefanie Düssler <s.dussler20@imperial.ac.uk>
2023-08-09 10:32:16 +0800

    fix [gust response] finalize plot script

--- 04_Gust_Response/plot_gust_response.py
+++ 04_Gust_Response/plot_gust_response.py
@@ -5,7 +5,7 @@ import os
 route_dir = os.path.abspath(os.path.dirname(os.path.realpath(__file__)))
 
 def plot_data(list_gust_lengths, list_gust_intensity, list_cfl1, list_polars, case_string_format,results_folder):
-    fig, axs = plt.subplots(4)
+    fig, axs = plt.subplots(4,figsize=(10,6))
     for i_gust_length, gust_length in enumerate(list_gust_lengths):
         for gust_intensity in list_gust_intensity:
             for cfl1 in list_cfl1:
@@ -18,24 +18,34 @@ def plot_data(list_gust_lengths, list_gust_intensity, list_cfl1, list_polars, ca
                         axs[iparameter].plot(data[1:,0], 
                                              data[1:,iparameter + 1], 
                                              color = list_colors[i_gust_length],
-                                             linestyle = get_linestyle(cfl1, polars), 
-                                             label= get_label(cfl1, polars, iparameter, gust_length))
+                                             linestyle = get_linestyle(cfl1, polars))
     for iax in range(len(axs)):
         plt.setp(axs[iax].get_xticklabels(), visible=iax == len(axs)-1)
         axs[iax].set_ylabel(list_ylabels[iax])
         axs[iax].set_xlim([0.,2.5])
+        axs[iax].grid()
+    handles = get_legend(list_gust_lengths)
+
     plt.xlabel("time, s")
     plt.tight_layout()
-    axs[0].legend(ncols=len(list_gust_lengths), loc='upper center', bbox_to_anchor=(0.7, 1.2))
-    plt.savefig(results_folder + 'dynamic_gust_response.png')
+    lgd = axs[1].legend(ncols=1, #len(list_gust_lengths), 
+                        loc='upper right', 
+                        handles = handles[:len(list_gust_lengths)],
+                        bbox_to_anchor=(1.0, 1.2))
+                        
+    lgd = axs[0].legend(ncols=2, loc='upper right', handles = handles[-2:],
+                        bbox_to_anchor=(1.0, 1.))
+    plt.savefig(results_folder + 'dynamic_gust_response.png', bbox_extra_artists=(lgd,), bbox_inches='tight')
     plt.show()
-                    
-def get_label(cfl1, polars, iparameter, gust_length):
-    if cfl1 or polars or iparameter >0:
-        return None
-    else:
-        return 'H = {} m'.format(gust_length)
-    
+
+def get_legend(list_gust_length):
+    from matplotlib.lines import Line2D
+    list_labels = []
+    for igust, gust_length in enumerate(list_gust_length):
+        list_labels.append(Line2D([0], [0],color=list_colors[igust], label = "H = {} m".format(gust_length)))
+    list_labels.append(Line2D([0], [0],color='k', linestyle='dashed', label='uniform wake discretisation')) 
+    list_labels.append(Line2D([0], [0],color='k', linestyle='solid', label='variable wake discretisation'))
+    return list_labels
 
 def get_linestyle(cfl1, polar):
     if bool(cfl1) and not bool(polar):
@@ -50,7 +60,7 @@ def main():
 
     list_gust_lengths = [5, 10, 20, 40, 80, 100]
     list_gust_intensity = [10]
-    list_cfl1 = [0, 1] 
+    list_cfl1 = [1, 0] 
     list_polars = [0] 
     plot_data(list_gust_lengths, list_gust_intensity, list_cfl1, list_polars, case_string_format, results_folder)
 
