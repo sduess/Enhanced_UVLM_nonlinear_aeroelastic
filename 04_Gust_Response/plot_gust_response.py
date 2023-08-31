e3a71318e9891c9f2a4b33af6c8f8f2807c89459
Stefanie Düssler <s.dussler20@imperial.ac.uk>
2023-08-30 22:16:55 -0400

    fix [gust response] plotting script for polars

--- 04_Gust_Response/plot_gust_response.py
+++ 04_Gust_Response/plot_gust_response.py
@@ -13,10 +13,9 @@ def plot_data(list_gust_lengths, list_gust_intensity, list_cfl1, list_polars, ca
                     file = results_folder +case_string_format.format(gust_length, gust_intensity, polars, cfl1) + '.txt'
                     data = np.loadtxt(file,
                                       delimiter=",")
-                    data[1:,1] /= 2
                     for iparameter in range(np.shape(data)[1] - 1):
-                        axs[iparameter].plot(data[1:,0], 
-                                             data[1:,iparameter + 1], 
+                        axs[iparameter].plot(data[2:,0], 
+                                             data[2:,iparameter + 1], 
                                              color = list_colors[i_gust_length],
                                              linestyle = get_linestyle(cfl1, polars))
     for iax in range(len(axs)):
@@ -24,7 +23,7 @@ def plot_data(list_gust_lengths, list_gust_intensity, list_cfl1, list_polars, ca
         axs[iax].set_ylabel(list_ylabels[iax])
         axs[iax].set_xlim([0.,2.5])
         axs[iax].grid()
-    handles = get_legend(list_gust_lengths)
+    handles = get_legend(list_gust_lengths, 1 in list_polars)
 
     plt.xlabel("time, s")
     plt.tight_layout()
@@ -38,30 +37,33 @@ def plot_data(list_gust_lengths, list_gust_intensity, list_cfl1, list_polars, ca
     plt.savefig(results_folder + 'dynamic_gust_response.png', bbox_extra_artists=(lgd,), bbox_inches='tight')
     plt.show()
 
-def get_legend(list_gust_length):
+def get_legend(list_gust_length, flag_polar_study=False):
     from matplotlib.lines import Line2D
     list_labels = []
     for igust, gust_length in enumerate(list_gust_length):
         list_labels.append(Line2D([0], [0],color=list_colors[igust], label = "H = {} m".format(gust_length)))
-    list_labels.append(Line2D([0], [0],color='k', linestyle='dashed', label='uniform wake discretisation')) 
-    list_labels.append(Line2D([0], [0],color='k', linestyle='solid', label='variable wake discretisation'))
+    if flag_polar_study:
+        list_labels.append(Line2D([0], [0],color='k', linestyle='dashed', label='SHARPy')) 
+        list_labels.append(Line2D([0], [0],color='k', linestyle='solid', label='SHARPy + Polar Corrections'))
+
+    else:
+        list_labels.append(Line2D([0], [0],color='k', linestyle='dashed', label='uniform wake discretisation')) 
+        list_labels.append(Line2D([0], [0],color='k', linestyle='solid', label='variable wake discretisation'))
     return list_labels
 
 def get_linestyle(cfl1, polar):
-    if bool(cfl1) and not bool(polar):
+    if bool(cfl1) or bool(polar):
         return '-'
-    elif bool(polar):
-        return ':'
     else:
         return '--'
 def main():         
     results_folder = route_dir + '/results_data/'
-    case_string_format =  'superflexop_free_gust_L_{:g}_I_{:g}_p_{:g}_cfl_{:g}'
+    case_string_format =  'superflexop_free_gust_L_{:g}_I_{:g}_p_{:g}_cfl_{:g}u_inf20'
 
-    list_gust_lengths = [5, 10, 20, 40, 80, 100]
+    list_gust_lengths = [5, 10, 20, 40, 80] #, 100]
     list_gust_intensity = [10]
-    list_cfl1 = [1, 0] 
-    list_polars = [0] 
+    list_cfl1 = [0] 
+    list_polars = [0,1] 
     plot_data(list_gust_lengths, list_gust_intensity, list_cfl1, list_polars, case_string_format, results_folder)
 
 
